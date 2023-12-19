16aa6c7489b62e4c2a4bfd16088653d186b9d558
Michal Wiktorowicz <michalwiktorowicz95@gmail.com>
2023-12-19 20:08:52 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kreator_nazw_czesci_EESA
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
